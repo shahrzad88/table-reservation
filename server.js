19175eb92cf4663192ee160165617e99f8cd5c80
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2018-12-12 21:00:32 -0500

    Added the files.

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Setting up the Express app
+var app = express();
+
+var port = process.env.PORT || 8080;
+// var port = 3000;
+
+// Setting up the Express app to handle data parsing
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.text());
+// app.use(express.json({ type: 'application/vnd.api+json' }));
+
+
+ app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+
+// The data
+var diners = [];
+
+// Routes
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/tables', function(req, res) {
+  res.sendFile(path.join(__dirname, 'tables.html'));
+});
+
+app.get('/reserve', function(req, res) {
+  res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+app.post('/api/tables', function(req, res) {
+  for (var i = 0; i < 4; i++) {
+    res.json(diners[i]);
+  }
+});
+
+app.get('/api/waitlist', function(req, res) {
+  for (var i = 5; i < diners.length; i++) {
+    res.json(diners[i]);
+  }
+});
+
+app.listen(port, function() {
+  console.log("I'm listening on PORT " + port);
+});
